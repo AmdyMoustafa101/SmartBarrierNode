b169e79fb80e16b425bfe000a75df840b8575a6b
AmdyMoustafa101 <taphisllefa@gmail.com>
2025-02-25 16:11:33 +0000

    plaques recherchées ok

--- models/Voiture.js
+++ models/Voiture.js
@@ -7,7 +7,7 @@ const voitureSchema = new mongoose.Schema({
   contact: {type: String, required: true},
   archived: { type: Boolean, default: false },
   estCible: { type: Boolean, default: false },
-  organisme: { type: String, enum: ['police', 'gendarmerie', 'ambulance'], required: true }
+  organisme: { type: String, enum: ['police', 'gendarmerie', 'ambulance', 'recherché'], required: true }
 });
 
 const Voiture = mongoose.model('Voiture', voitureSchema);
