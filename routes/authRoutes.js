fc9ccbc1536ca0312a71bd4da9b1bda26896e2f4
KingOuze <ouzealdiey24@gmail.com>
2025-02-27 16:57:33 +0000

    envoie des mails

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const User = require('../models/User'); // Assurez-vous d'importer votre modèle User
 const RevokedToken = require('../models/RevokedToken'); // Importer le modèle RevokedToken
 const router = express.Router();
@@ -29,10 +30,37 @@ const authMiddleware = async (req, res, next) => {
   }
 };
 
+// Fonction pour envoyer un email
+async function sendEmail(user, plainPassword) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'taphisllefa@gmail.com', // Remplacez par votre email
+      pass: 'tfkc hian kkid igqs' // Remplacez par votre mot de passe
+    }
+  });
+
+  const mailOptions = {
+    from: 'taphisllefa@gmail.com',
+    to: user.email,
+    subject: 'Vos informations de connexion à SmartBarrier',
+    text: `Bonjour ${user.prenom} ${user.nom},\n\nVos informations de connexion sont les suivantes :\n\nNom : ${user.nom}\nPrénom : ${user.prenom}\nTéléphone : ${user.telephone}\nMot de passe : ${plainPassword}\nRôle : ${user.role}\nOrganisme : ${user.organisme}\n\nCordialement,\nVotre équipe`
+  };
+
+  await transporter.sendMail(mailOptions);
+}
+
+// Fonction pour générer un mot de passe
+function generatePassword(nom, prenom) {
+  const randomDigits = Math.floor(1000 + Math.random() * 9000); // Génère 4 chiffres aléatoires
+  return `${nom}${prenom}@${randomDigits}`;
+}
+
 // Route de connexion
 router.post('/login', async (req, res) => {
   const { email, motDePasse } = req.body;
 
+ 
   try {
     const user = await User.findOne({ email });
     if (!user) {
@@ -62,9 +90,9 @@ router.post('/login', async (req, res) => {
 // Ajoutez cette route dans votre fichier de routes
 router.post('/forgot-password', async (req, res) => {
   const { email } = req.body;
-
+  console.log(email);
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({  email });
     if (!user) {
       return res.status(404).json({ message: 'Aucun utilisateur trouvé avec cet email.' });
     }
@@ -76,6 +104,7 @@ router.post('/forgot-password', async (req, res) => {
 
     res.status(200).json({ message: 'Un nouveau mot de passe a été envoyé à votre email.' });
   } catch (error) {
+    console.error('Forgot Password Error:', error);
     res.status(500).json({ message: 'Erreur serveur' });
   }
 });
